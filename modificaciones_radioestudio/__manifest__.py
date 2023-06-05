6e12563307b5c3fb95a0800859b74ba2bb8f7f34
jgargallo <jgargallo@zitycard.com>
2023-06-05 15:29:26 +0200

    Módulo modificaciones

---
+++ modificaciones_radioestudio/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+	"name": "Modificaciones Radioestudio",
+	"summary": "Personaliza el odoo a las necesidades de Radioestudio",
+	"description": "Añade las funcionalidades requeridas por Radioestudio.",
+	"author": "Zitycard",
+	"website": "https://zitycard.com",
+	"version": "0.1",
+	"depends": ["base", "account_accountant"],
+	"data": [
+		"views/radioestudio.xml"
+	]
+}
